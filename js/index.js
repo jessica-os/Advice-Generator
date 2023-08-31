fc9c0af07e481bec12ffe4de2a73f4fce58522e0
Jessica Oliveira <elisa0js@gmail.com>
2023-08-31 12:11:38 -0300

    refatoracao JS

--- js/index.js
+++ js/index.js
@@ -3,38 +3,12 @@ const id = document.querySelector("span");
 const description = document.querySelector("#description");
 
 button.addEventListener("click", () => {
-  gerarUmId();
-  gerarUmAdvice();
+  geradorDeConselhos();
 });
 
 async function geradorDeConselhos() {
-  const url = "https://api.adviceslip.com/advice";
-  const resposta = await fetch(url);
-  return await resposta.json();
+  const url = await fetch("https://api.adviceslip.com/advice");
+  const resposta = await url.json();
+  id.innerHTML = `#${resposta.slip.id}`;
+  description.innerHTML = `"${resposta.slip.advice}"`;
 }
-
-async function gerarId(slip_id) {
-  const url = `https://api.adviceslip.com/advice/${slip_id}`;
-  const resposta = await fetch(url);
-  return await resposta.json();
-}
-
-async function gerarUmId() {
-  const conselhos = await geradorDeConselhos();
-  const conselho = await gerarId(conselhos.slip.id);
-  const oConselho = conselho.slip.id;
-  id.innerHTML = `# ${oConselho}`;
-}
-
-async function gerarAdvice(advice) {
-  const url = `	https://api.adviceslip.com/advice/search/${advice}`;
-  const resposta = await fetch(url);
-  return await resposta.json();
-}
-async function gerarUmAdvice() {
-  const conselhos = await geradorDeConselhos();
-  const conselho = await gerarAdvice(conselhos.slip.advice);
-  const oAdvice = conselho.query;
-  description.innerHTML = oAdvice;
-}
-
